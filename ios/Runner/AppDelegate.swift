9f93df1155dda233db3109b45fc241d8514d8ac0
Tomasz Siudut - AppLover <tomaszsiudut-applover@Tomaszs-MacBook-Pro.local>
2022-06-29 15:45:18 +0200

    native plugin finished

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,26 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    activateScreenProtection()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    var field = UITextField()
+    
+    private func activateScreenProtection() {
+      if (!window.subviews.contains(field)) {
+        window.addSubview(field)
+        field.centerYAnchor.constraint(equalTo: window.centerYAnchor).isActive = true
+        field.centerXAnchor.constraint(equalTo: window.centerXAnchor).isActive = true
+        window.layer.superlayer?.addSublayer(field.layer)
+        field.layer.sublayers?.first?.addSublayer(window.layer)
+    }
+}
+    override func applicationWillResignActive(_ application: UIApplication) {
+      field.isSecureTextEntry = false
+    }
+
+    override func applicationDidBecomeActive(_ application: UIApplication) {
+      field.isSecureTextEntry = true
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:screenshot_blocker/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
